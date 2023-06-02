c612c99ea15769ce3e9c05316f4f3d3a754874b1
Ewa-M <92916895+Ewa-M@users.noreply.github.com>
2023-06-02 23:51:17 +0200

    CPBP-85 path implementation

--- src/main/kotlin/pl/edu/agh/path/domain/PathRequest.kt
+++ src/main/kotlin/pl/edu/agh/path/domain/PathRequest.kt
@@ -7,4 +7,4 @@ import pl.edu.agh.shoppingList.domain.ShoppingListId
  * Request for calculating path for shopping list
  */
 @Serializable
-data class PathRequest(val shoppingListId: ShoppingListId, val longitude: Double, val latitude: Double)
+data class PathRequest(val shoppingListId: ShoppingListId, val longitude: Double, val latitude: Double, val fewestShops: Boolean?)
