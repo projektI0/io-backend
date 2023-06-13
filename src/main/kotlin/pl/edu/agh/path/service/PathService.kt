c884ea5d2953a91ae598a47bdb69f2761fa74d8f
Ewa-M <92916895+Ewa-M@users.noreply.github.com>
2023-06-13 19:46:29 +0200

    Cpbp 85 path calculation (#28)
    * CPBP-85 path implementation
    
    * CPBP-85 minor refactor
    
    * CPBP-85 code review

--- src/main/kotlin/pl/edu/agh/path/service/PathService.kt
+++ src/main/kotlin/pl/edu/agh/path/service/PathService.kt
@@ -6,6 +6,7 @@ import pl.edu.agh.path.domain.PathRequest
 import pl.edu.agh.path.domain.PathResponse
 import pl.edu.agh.shop.dao.ShopDao
 import pl.edu.agh.shop.domain.dto.ShopTableDTO
+import pl.edu.agh.tag.domain.TagId
 import kotlin.math.sqrt
 
 class Point(private val x: Double, private val y: Double) {
@@ -29,40 +30,181 @@ class PathServiceImpl : PathService {
             ShopDao.getAllShopsWithinBounds(startY - 5, startX - 5, startY + 5, startX + 5, userId).map { it.id }
                 .toSet()
         // Create a set of all categories we need to buy
-        val remainingTags = PathDao.getAllTopTagsForList(shoppingListId)
+        val allTags: MutableSet<TagId> = PathDao.getAllTopTagsForList(shoppingListId)
 
         // Create a map of the shops by tags
-        val shopsByTags = PathDao.getShopsForTags(shopsIds, remainingTags)
+        val shopsByTags: Map<TagId, Set<ShopTableDTO>> = PathDao.getShopsForTags(shopsIds, allTags)
 
         // Create a map of the tags by shops
-        val tagsByShops = PathDao.getTagsForShops(shopsIds)
+        val tagsByShops: Map<ShopTableDTO, Set<TagId>> = PathDao.getTagsForShops(shopsIds)
+
+        val (availableTags, remainingTags) = allTags.partition { shopsByTags.containsKey(it) }
 
         // Initialize the starting point as the current location
-        var currentPoint = Point(startX, startY)
+        var currentPoint: Point = Point(startX, startY)
 
-        // Create a list to store the shops we need to visit
-        val route = mutableListOf<ShopTableDTO>()
+        val route: MutableList<ShopTableDTO>
 
-        while (remainingTags.isNotEmpty()) {
-            // Find the nearest shop that sells one of the remaining categories
-            val nearestShop = shopsByTags.filterKeys { remainingTags.contains(it) }.values.flatten()
-                .minByOrNull { currentPoint.distance(Point(it.longitude, it.latitude)) }
+        if (pathRequest.fewestShops == true) {
+            route = fewestShopsPath(tagsByShops, currentPoint, availableTags.toMutableSet())
+        } else {
+            route = shortestPath(shopsByTags, tagsByShops, currentPoint, availableTags.toMutableSet())
+        }
 
-            if (nearestShop is ShopTableDTO) {
-                // Update current location and remove the category from the set of remaining categories
-                currentPoint = Point(nearestShop.longitude, nearestShop.latitude)
-                tagsByShops[nearestShop]?.forEach {
-                    remainingTags.remove(it)
-                }
+        return PathResponse(route, remainingTags.toSet())
+    }
 
-                // Add the nearest shop to the route
-                route.add(nearestShop)
+    private fun shortestPath(
+        shopsByTags: Map<TagId, Set<ShopTableDTO>>,
+        tagsByShops: Map<ShopTableDTO, Set<TagId>>,
+        startingPoint: Point,
+        tags: MutableSet<TagId>
+    ): MutableList<ShopTableDTO> {
+        var path: MutableList<ShopTableDTO>
+        var pointPath: MutableList<Point>
+        var shops: MutableSet<ShopTableDTO> = mutableSetOf()
+
+        shops = tags.flatMap { tag ->
+            shopsByTags[tag]?:emptySet()
+        }.toMutableSet()
+
+        //find all shops that need to be included (one of a kind)
+        var necessaryShops: MutableSet<ShopTableDTO> = mutableSetOf()
+        shopsByTags.entries
+            .filter { tags.contains(it.key) }
+            .filter { it.value.size == 1 }
+            .forEach { necessaryShops.addAll(it.value) }
+
+
+        if (necessaryShops.isNotEmpty()) {
+            val (sortedRoute, sortedPointRoute) = sortShops(startingPoint, necessaryShops)
+            path = sortedRoute
+            pointPath = sortedPointRoute
+            shops.removeIf { necessaryShops.contains(it) }
+        } else {
+            val firstShop = shopClosestToPoint(startingPoint, shops)
+            path = mutableListOf(firstShop)
+            pointPath = mutableListOf(startingPoint, Point(firstShop.longitude, firstShop.latitude))
+            shops.remove(firstShop)
+        }
+
+        path.forEach { shop -> tags.removeIf { !(tagsByShops[shop]?: emptySet()).contains(it) } }
+
+        while (tags.isNotEmpty()) {
+            val (index, newShop) = findOptimalShop(pointPath, shops)
+            val newShopTags = tagsByShops[newShop]
+
+            path.add(index, newShop)
+            pointPath.add(index, Point(newShop.longitude, newShop.latitude))
+
+            //remove all doubles (new shop contains tags added to path earlier)
+            val removeShopsAtIndex = mutableListOf<Int>()
+            path.withIndex().forEach {
+                if (newShopTags?.containsAll(tagsByShops[it.value]?: emptySet()) == true && it.value != newShop)
+                    removeShopsAtIndex.add(it.index)
+            }
+
+            removeShopsAtIndex.asReversed().forEach {
+                path.removeAt(it)
+                pointPath.removeAt(it)
+            }
+
+            tags.removeIf { newShopTags?.contains(it)?: false }
+        }
+
+        return path;
+    }
+
+    private fun sortShops(startingPoint: Point, shops: MutableSet<ShopTableDTO>): Pair<MutableList<ShopTableDTO>, MutableList<Point>> {
+        var path: MutableList<ShopTableDTO> = mutableListOf()
+        var pointPath = mutableListOf(startingPoint)
+
+        val startingShop = shopClosestToPoint(startingPoint, shops)
+        path.add(startingShop)
+        pointPath.add(Point(startingShop.longitude, startingShop.latitude))
+
+        while(shops.isNotEmpty()) {
+            val (index, shop) = findOptimalShop(pointPath, shops)
+            path.add(index, shop)
+            pointPath.add(index, Point(shop.longitude, shop.latitude))
+            shops.remove(shop)
+        }
+
+        return Pair(path, pointPath)
+    }
+
+    private fun fewestShopsPath(
+        tagsByShops: Map<ShopTableDTO, Set<TagId>>,
+        startingPoint: Point,
+        tags: MutableSet<TagId>
+    ): MutableList<ShopTableDTO> {
+        val route= mutableListOf<ShopTableDTO>()
+        val pointRoute = mutableListOf(startingPoint)
+
+        while (tags.isNotEmpty()) {
+            val bestShops = shopsWithMostTags(tagsByShops, tags)
+
+            if (bestShops.isNotEmpty()) {
+                val (index, shop) = findOptimalShop(pointRoute, bestShops)
+                route.add(index, shop)
+                pointRoute.add(index, Point(shop.longitude, shop.latitude))
+                tags.removeIf { !(tagsByShops[shop]?: emptySet()).contains(it) }
             } else {
-                // No more shops selling the remaining categories
-                break
+                break;
+            }
+        }
+        return route;
+    }
+
+    private fun findOptimalShop(currentPath: List<Point>, potentialShops: Set<ShopTableDTO>): Pair<Int, ShopTableDTO> {
+        var minDistance = Double.MAX_VALUE
+        var bestShop = potentialShops.first()
+        var indexToInsert = 1
+
+        if (currentPath.size == 1) {
+            bestShop = shopClosestToPoint(currentPath[0], potentialShops)
+        } else {
+            for (potential in potentialShops) {
+                for ((index, point) in currentPath.withIndex()) {
+                    if (index == 0) continue
+
+                    var currentDistance = distanceFromShopToPoint(potential, point)
+                    currentDistance += distanceFromShopToPoint(potential, currentPath[index-1])
+
+                    if (currentDistance < minDistance) {
+                        indexToInsert = index
+                        minDistance = currentDistance
+                        bestShop = potential
+                    }
+                }
             }
         }
 
-        return PathResponse(route, remainingTags)
+        return Pair(indexToInsert, bestShop)
+    }
+
+    private fun shopClosestToPoint(point: Point, shops: Set<ShopTableDTO>): ShopTableDTO {
+        return shops.minBy { point.distance(Point(it.longitude, it.latitude)) }
+    }
+
+    private fun distanceFromShopToPoint(shop: ShopTableDTO, point: Point): Double {
+        return point.distance(Point(shop.longitude, shop.latitude))
+    }
+
+    private fun shopsWithMostTags(tagsByShops: Map<ShopTableDTO, Set<TagId>>, remainingTags: MutableSet<TagId>):
+            MutableSet<ShopTableDTO> {
+        var maxTags = 0
+        var maxTagsShops = mutableSetOf<ShopTableDTO>()
+
+        for (entry in tagsByShops.entries.iterator()) {
+            var noOfTags = entry.value.filter { remainingTags.contains(it) }.size
+            if (noOfTags > maxTags) {
+                maxTags = noOfTags
+                maxTagsShops = mutableSetOf<ShopTableDTO>(entry.key)
+            } else if (noOfTags == maxTags) {
+                maxTagsShops.add(entry.key)
+            }
+        }
+        return maxTagsShops
     }
 }
